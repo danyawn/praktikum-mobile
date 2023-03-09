af94d752e99a3747519d9a91e4adce5503d17167
Dan Yawn <thephantomwarrior02@gmail.com>
2023-03-10 05:39:03 +0700

    tugas1 praktikum mobile

--- tugas1/lib/loginpage.dart
+++ tugas1/lib/loginpage.dart
@@ -14,56 +14,59 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: Center(child: Text('Login')),
 
       ),
-      body: Center(
-        child: Container(
-          width: 500,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 50),
-                child: Icon(Icons.person,
-                size: 150,),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      labelText: "Email"
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Center(
+          child: Container(
+            width: 500,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 50),
+                  child: Icon(Icons.person,
+                  size: 150,),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        labelText: "Username"
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      labelText: "Password"
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        labelText: "Password"
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(30),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    child: Text("Login"),
+                    onPressed: () {
+
+                    },
                   ),
-                  child: Text("Login"),
-                  onPressed: () {
-                    
-                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
